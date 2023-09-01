987cb1570f280a92753ec99fea91a5a6abc817ac
Igor <sergienko339@gmail.com>
2023-09-01 23:59:49 +0300

    possible fixes

--- firebase/functions/src/evn.ts
+++ firebase/functions/src/evn.ts
@@ -3,8 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const ENV = {
-  replicatie_api_token: process.env.REPLICATE_API_TOKEN,
-  replicate_model: process.env.REPLICATE_MODEL,
+    replicatie_api_token: process.env.REPLICATE_API_TOKEN,
+    replicate_model: process.env.REPLICATE_MODEL,
+    fb_bucket: process.env.FB_BUCKET,
 };
 
 export default ENV;
