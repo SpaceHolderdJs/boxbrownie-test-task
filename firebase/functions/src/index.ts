27c83b3f1d6f6a01cf33fc88a659560552ecf1fc
Igor <sergienko339@gmail.com>
2023-09-01 23:59:49 +0300

    possible fixes

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,35 +1,38 @@
 import { onObjectFinalized } from "firebase-functions/v2/storage";
-import * as logger from "firebase-functions/logger";
 import { onRequest } from "firebase-functions/v1/https";
-import replicate from "./replicate.config";
+import { apps, initializeApp, database } from "firebase-admin";
+import * as logger from "firebase-functions/logger";
 import ENV from "./evn";
+import replicate from "../../../replicate.config";
+// import replicate from "./replicate.config";
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.log("Hello logs!");
+if (apps.length === 0) {
+    initializeApp({ storageBucket: ENV.fb_bucket });
+}
 
-  response.send("Hello from Firebase!");
+exports.helloWorld = onRequest((_, response) => {
+    logger.log("Hello logs!");
+
+    response.send("Hello from Firebase!");
 });
 
 exports.handleImageUpload = onObjectFinalized(
-  { bucket: "boxbrownie-f3421-u78nq" },
-  async (event) => {
-    console.log(event, "event");
-    logger.info(
-      event,
-      "event",
-      `https://storage.googleapis.com/${event.data.bucket}/${event.data.name}`
-    );
+    { bucket: ENV.fb_bucket },
+    (event) => {
+        const input = {
+            image: event.data.mediaLink,
+            prompt: "a picture of house at night",
+            scale: 0.5,
+        };
 
-    const input = await replicate.run(ENV.replicate_model as any, {
-      input: {
-        image: event.data.mediaLink,
-        prompt: "a picture of house an night",
-        scale: 0.5,
-      },
-    });
-
-    const output = await replicate.run(ENV.replicate_model as any, { input });
-
-    return output;
-  }
+        replicate
+            .run(ENV.replicate_model as any, {
+                input,
+            })
+            .then((output: {}) =>
+                database()
+                    .ref()
+                    .push({ filename: event.data.name, url: output }),
+            );
+    },
 );
